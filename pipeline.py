45a1d0767b5ba7d995f837227536ff976a57b2e4
Sr Marinho <37448831+juanengml@users.noreply.github.com>
2021-09-20 16:47:12 -0300

    Add files via upload

---
+++ pipeline.py
@@ -0,0 +1,26 @@
+import papermill as pm
+
+from console_logging.console import Console
+console = Console()
+
+console.log("RUNNING NOTEBOOK 01")
+pm.execute_notebook(
+   'Machine Learning Pipeline - 01.ipynb',
+    "out1.ipynb"
+)
+
+console.log("RUNNING NOTEBOOK 02")
+pm.execute_notebook(
+   'Machine Learning Pipeline - 02.ipynb',
+    "out2.ipynb"
+)
+
+console.info("RUNNING NOTEBOOK 03")
+pm.execute_notebook(
+   'Machine Learning Pipeline - 03.ipynb',
+    "out3.ipynb"
+)
+
+
+console.success("DONe! ")
+
